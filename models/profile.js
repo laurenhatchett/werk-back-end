0689850295c00603f7430fcb19f420b800daee01
Leon Chu <Ljchu87@gmail.com>
2022-10-21 14:20:44 -0500

    Add myLogSchema

--- models/profile.js
+++ models/profile.js
@@ -2,6 +2,21 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const myLogSchema = new Schema ({
+  date: {
+    type: Number,
+    required: true
+  },
+  logEntry: {
+    type: String,
+    required: true
+  },
+  skills: String,
+  private: Boolean
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
   photo: String
