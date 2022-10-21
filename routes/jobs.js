c04224c7d5a7aae3e1b375ea3039c55ad09bb2ac
AlyssaD <pink_lime@live.com>
2022-10-21 15:26:58 -0400

    add routes and controller

---
+++ routes/jobs.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import * as jobsCtrl from '../controllers/jobs.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, jobsCtrl.index)
+router.get('/:id', checkAuth, jobsCtrl.show)
+router.put('/:id', checkAuth, jobsCtrl.update)
+router.post('/', checkAuth, jobsCtrl.create)
+router.delete('/:id', checkAuth, blogsCtrl.delete)
+
+export { router }
