a9f7c92f62071dc93a900cb0cbd9cbd339afba9e
laurenhatchett <laurenhatchett@gmail.com>
2022-10-21 15:21:47 -0400

    write resource model

---
+++ models/resource.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const resourceSchema = new Schema (
+  {
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  URL: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: []
+  },
+  skills: {
+    type: String,
+    required: true,
+  },
+  owner:[{ObjectId ref:'Profile'}]
+  }
+) 
