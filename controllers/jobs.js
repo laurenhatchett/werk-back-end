f6ffc4a36d6e3b980347738d7d69ee5c0a776601
AlyssaD <pink_lime@live.com>
2022-10-22 17:19:33 -0400

    add create and index functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,8 +1,48 @@
 import { Profile } from "../models/profile.js"
 import { Job } from "../models/job.js"
 
+const create = async (req, res) => {
+  try {
+    const job = await Job.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { jobs: job } },
+      { new: true }
+    )
+    res.status(201).json(job)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
 
+const index = async (req, res) => {
+  try {
+    const jobs = await Job.find({})
+    .sort({ createdAt: 'desc' })
+  res.status(200).json(jobs)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
+const show = async (req, res) => {
+
+}
+
+const update = async (req, res) => {
+
+}
+
+const deleteJob = async (req, res) => {
+
+}
 
 export {
-
+  index,
+  create,
+  show,
+  update,
+  deleteJob,
 }
